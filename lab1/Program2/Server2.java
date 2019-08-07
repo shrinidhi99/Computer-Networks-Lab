27fb258cef64789f709caf0b7c75cfaf97d6f37d
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-08-07 16:06:13 +0530

    added folders

--- lab1/Program2/Server2.java
+++ lab1/Program2/Server2.java
@@ -26,7 +26,7 @@ public class Server2
 				DataInputStream dis = new DataInputStream(s.getInputStream());
 				DataOutputStream dos = new DataOutputStream(s.getOutputStream());
 				System.out.println("Assigning new thread for this client...");
-				Thread t = new ClientHandler(s,dis,dos);
+				Thread t = new ClientHandler(s,dis,dos,"Hello");
 				t.start();
 			}
 			catch(Exception e)
@@ -42,25 +42,30 @@ class ClientHandler extends Thread
 { 
     final DataInputStream dis;
     final DataOutputStream dos;
+    final String serverTalk;
     final Socket s;
     DateFormat fordate = new SimpleDateFormat("yyyy/MM/dd");
     DateFormat fortime = new SimpleDateFormat("hh:mm:ss");
   
     // Constructor 
-    public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos) 
+    public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos, String serverTalk) 
     { 
         this.s = s;
         this.dis = dis;
         this.dos = dos;
+        this.serverTalk = serverTalk;
     }
   
     @Override
     public void run()
     { 
         String received;
+        // String serverTalk = "";
         String toreturn;
+        // BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         while (true)
         { 
+            // serverTalk = "";
             try {
   
                 // Ask user what he wants 
@@ -68,9 +73,10 @@ class ClientHandler extends Thread
                   
                 // receive the answer from client
                 received = dis.readUTF();
-                  
+                // serverTalk = in.readLine();
                 if(received.equals("Exit"))
                 {  
+                    // System.out.println(serverTalk);
                     System.out.println("Client " + this.s + " sends exit...");
                     System.out.println("Closing this connection.");
                     this.s.close();
@@ -83,6 +89,9 @@ class ClientHandler extends Thread
                   
                 // write on output stream based on the 
                 // answer from the client 
+                // System.out.println(serverTalk);
+                // System.out.println(received);
+                
                 switch (received) {
                   
                     case "Date" :
@@ -96,9 +105,10 @@ class ClientHandler extends Thread
                         break;
                           
                     default:
-                        dos.writeUTF("received");
+                        dos.writeUTF(received);
                         break;
                 }
+                // dos.writeUTF(serverTalk);
                 // dos.writeUTF(received); 
                         
                           
@@ -111,7 +121,8 @@ class ClientHandler extends Thread
         try
         { 
             // closing resources 
-            this.dis.close(); 
+            this.dis.close();
+            // in.close();
             this.dos.close(); 
               
         }catch(IOException e){ 
